543b3874eefa29c30b156da346e69e0bad02471c
Timothy Zeb Girouard <timzebgir@gmail.com>
2016-04-25 23:21:40 +0000

    I think heroku doesn't like the process.env.IP piece...

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ routes(router);
 
 var server = http.createServer(router);
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 3000, function(){
   var addr = server.address();
-  console.log("Server listening at", addr.address + ":" + addr.port);
+  console.log("Server listening on port: " + addr.port);
 });
